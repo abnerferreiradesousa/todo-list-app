f19ed11eedb8b7d6eedd68a8f8a588b19c42b428
Abner <abnerferreira84@gmail.com>
2023-10-24 10:51:04 -0300

    feat: Add handler de erros.

--- todo-list-backend-ivipcoin/src/app.ts
+++ todo-list-backend-ivipcoin/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import taskRouter from './routes/task';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json());
 app.use('/tasks', taskRouter);
+app.use(errorHandler);
 
 export default app;
---
+++ todo-list-backend-ivipcoin/src/middlewares/errorHandler.ts
@@ -0,0 +1,30 @@
+import { ZodError } from "zod";
+import { Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
+import { ErrorTypes, errorCatalog } from '../errors/catalog';
+
+
+const errorHandler: ErrorRequestHandler = (
+  err: Error | ZodError,
+  _req: Request,
+  res: Response,
+  _next,
+) => {
+
+  if(err instanceof ZodError) {
+    return res.status(400).json({ messge: err.issues });
+  }
+
+  const messageAsErrorTYpe = err.message as keyof typeof ErrorTypes;
+  const mappedError = errorCatalog[messageAsErrorTYpe];
+
+  if(mappedError) {
+    const { httpStatus, error } = mappedError;
+    return res.status(httpStatus).json({ error });
+  }
+
+  console.error(err);
+  return res.status(500).json({ message: err.message });
+}
+
+export default errorHandler;
