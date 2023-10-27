74479c9addea0d12219fc1f45ca3e07c02f69d9e
Abner <abnerferreira84@gmail.com>
2023-10-27 01:21:27 -0300

    feat: ajustando link do backend

--- todo-list-frontend-ivipcoin/src/app/global.ts
+++ todo-list-frontend-ivipcoin/src/app/global.ts
@@ -1,3 +1,6 @@
 require("dotenv").config();
 
-export const TODO_API_URI = process.env.TODO_API_URI || 'http://localhost:8000';
+export const TODO_API_URI = process.env.TODO_API_URI || 'https://todo-list-ofc.onrender.com';
+
+
+// Se usar Docker troque a URL acima à direita por essa: 'http://localhost:8000'
